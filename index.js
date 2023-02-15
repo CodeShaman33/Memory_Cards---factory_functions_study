fcbc998c9b4599f793c7cfda5d3e12d9800f1c9c
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2023-02-15 13:41:17 +0100

    card factory added

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
+let array = [
+    {
+        name: 'obj1',
+        backImage: 'src/photos/photo1.jpg',
+        matched: false,
+        id: 1
+    }
+]
 
 
-
-function createCard(cardData)
-{
+function createCard(cardData) {
     const card = cardFactory(cardData);
     const element = document.createElement('div');
     element.classList.add('card');
+    element.id = cardData.id;
     element.dataset.name = card.name;
 
     // front side of the card
     const front = document.createElement('div');
     front.classList.add('card-front');
-    front.style.backgroundImage = `url(${card.frontImage})`;
+    front.style.backgroundColor = 'red';
+    //front.innerHTML = cardData.name;
+    front.innerHTML = 'front';
+    front.style.display = 'block';
 
+    const back = document.createElement('div');
+    back.classList.add('card-back');
+    //back.style.backgroundImage = cardData.backImage;
+    back.innerHTML = 'back';
+    back.style.display = 'none';
+
+    element.appendChild(front);
+    element.appendChild(back);
+
+    // event listener
+    element.addEventListener('click', function(){flipCard(element);});
+    
+    return element;
 
 }
+
+function cardFactory(cardData) {
+    return {
+      name: cardData.name,
+      backImage: cardData.backImage,
+      matched: false
+    };
+  }
+
+  function createSet()
+  {
+    let parrent = document.getElementById('container');
+    parrent.appendChild(createCard(array[0]));
+  }
+
+  document.getElementById('start').addEventListener('click', function(){createSet();});
+
+document.get
